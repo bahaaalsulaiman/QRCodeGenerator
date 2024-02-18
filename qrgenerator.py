6c4d52d47eab40fa100dc1ae228fa014fb25788e
Bahaa Alsulaiman <bahaaalsulaiman@icloud.com>
2024-02-18 22:29:43 +0300

    Update qrgenerator.py

--- qrgenerator.py
+++ qrgenerator.py
@@ -3,15 +3,15 @@ from PIL import Image
 
 qr = qrcode.QRCode(
     version = 1,
-    error_correction = qrcode.ERROR_CORRECT_L,
+    error_correction = qrcode.ERROR_CORRECT_L,   #recomanded to check the documntation of this library
     box_size = 10,
     border=4,
 
 )
 
 
-qr.add_data("https://github.com/bahaaalsulaiman")
+qr.add_data("Here you put the link of a website or wep page")
 qr.make(fit=True)
 
-img = qr.make_image(fill_color = "green", back_color="white")
-img.save("bahaa_p.png")
+img = qr.make_image(fill_color = "green", back_color="white") # specifies the color of theqr and the background color
+img.save("bahaa_p.png") #here you save the qr code in the folder of the project
